c3063f2542119e73dbf8e5181bf2c61bac781259
badruzzaman <badruzzaman@20220920CLONE>
2023-01-31 15:27:00 +0600

    Add project files.

---
+++ Two Number Sum/Program.cs
@@ -0,0 +1,31 @@
+﻿
+
+using System.ComponentModel.Design;
+
+int[] arr = { 1, 2, 3, 4, 5, 6 };
+int target = 9;
+int[] result = solution(arr, target);
+foreach (var item in result)
+{
+	Console.Write(item + " ");
+}
+Console.ReadKey();
+
+int[] solution(int[] arr, int target)
+{
+    HashSet<int> set = new HashSet<int>();
+	for (int i = 0; i < arr.Length; i++)
+	{
+		int currentNum = arr[i];
+		int potentialNum = target - currentNum;
+		if (set.Contains(potentialNum))
+		{
+			return new int[] { potentialNum, currentNum };
+		}
+		else
+		{
+			set.Add(currentNum);
+		}
+    }
+	return new int[2];
+}
