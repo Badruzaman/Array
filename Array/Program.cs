e202b6096f12482efe6d18b6a7bf9271efda0b56
Badruzzaman <badru.cse@gmail.com>
2023-01-31 20:07:07 +0600

    new changes

--- Array/Program.cs
+++ Array/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+
+for (int i = 0; i < 10; i++)
+{
+    Console.Write(i + " ");
+}
+Console.ReadKey();
--- Binary Search/Program.cs
+++ Binary Search/Program.cs
@@ -1,11 +1,29 @@
 ﻿
 int[] arr = { 1, 2, 3, 4, 5, 6 };
-int target = 5;
+int target = 6;
 int result = solution(arr, target);
 Console.Write(result);
 Console.ReadKey();
 
 int solution(int[] arr, int target)
 {
+    int left = 0;
+    int right = arr.Length - 1;
+    while (left <= right)
+    {
+        int mid = (left + right) / 2;
+        if (arr[mid] == target)
+        {
+            return mid;
+        }
+        else if (arr[mid] < target)
+        {
+            left++;
+        }
+        else
+        {
+            right--;
+        }
+    }
     return -1;
 }
