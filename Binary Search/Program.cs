82dfa19dde160ce942dbbbecd2d5a251ccaeb200
badruzzaman <badruzzaman@20220920CLONE>
2023-01-31 15:57:21 +0600

    changes

--- Binary Search/Program.cs
+++ Binary Search/Program.cs
@@ -1,28 +1,11 @@
 ﻿
 int[] arr = { 1, 2, 3, 4, 5, 6 };
 int target = 5;
-int[] result = solution(arr, target);
-foreach (var item in result)
-{
-    Console.Write(item + " ");
-}
+int result = solution(arr, target);
+Console.Write(result);
 Console.ReadKey();
 
-int[] solution(int[] arr, int target)
+int solution(int[] arr, int target)
 {
-    HashSet<int> set = new HashSet<int>();
-    for (int i = 0; i < arr.Length; i++)
-    {
-        int currentNum = arr[i];
-        int potentialNum = target - currentNum;
-        if (set.Contains(potentialNum))
-        {
-            return new int[] { potentialNum, currentNum };
-        }
-        else
-        {
-            set.Add(currentNum);
-        }
-    }
-    return new int[2];
+    return -1;
 }
