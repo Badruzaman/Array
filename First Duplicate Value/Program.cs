25aee625c8e46f0fc58cef3862e639f09893ba4d
badruzzaman <badruzzaman@20220920CLONE>
2023-02-12 12:15:21 +0600

    df

--- First Duplicate Value/Program.cs
+++ First Duplicate Value/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+
+int[] array = { 2, 1, 5, 3, 3, 2, 4 };
+int result = solution(array);
+Console.WriteLine(result);
+Console.ReadKey();
+
+int solution(int[] array)
+{
+    HashSet<int> seen = new HashSet<int>();
+    foreach (var item in array)
+    {
+        if (seen.Contains(item))
+        {
+            return item;
+        }
+        seen.Add(item);
+    }
+    return -1;
+}
