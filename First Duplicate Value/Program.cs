cd9e25ea1fea095172f1e80d0baccc75721f945c
badruzzaman <badruzzaman@20220920CLONE>
2023-02-12 15:39:37 +0600

    dfg

--- First Duplicate Value/Program.cs
+++ First Duplicate Value/Program.cs
@@ -2,11 +2,35 @@
 
 
 int[] array = { 2, 1, 5, 3, 3, 2, 4 };
-int result = solution(array);
+int result = solution_02(array);
 Console.WriteLine(result);
 Console.ReadKey();
 
+// time O(N^2) Space O(1)
 int solution(int[] array)
+{
+    int minimumSecondIndex = array.Length;
+    for (int i = 0; i < array.Length; i++)
+    {
+        int value = array[i];
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            int secondMatchValue = array[j];
+            if (value == secondMatchValue)
+            {
+                minimumSecondIndex = Math.Min(minimumSecondIndex, j);
+            }
+        }
+    }
+    if (minimumSecondIndex == array.Length)
+    {
+        return -1;
+    }
+    return array[minimumSecondIndex];
+}
+
+// time O(N) Space O(N)
+int solution_01(int[] array)
 {
     HashSet<int> seen = new HashSet<int>();
     foreach (var item in array)
@@ -16,6 +40,21 @@ int solution(int[] array)
             return item;
         }
         seen.Add(item);
+    }
+    return -1;
+}
+// time O(N) Space O(1)
+int solution_02 (int[] array)
+{
+    // { 2, 1, 5, 3, 3, 2, 4 };
+    foreach (var value in array)
+    {
+        int absValue = Math.Abs(value);
+        if (array[absValue - 1] < 0)
+        {
+            return absValue;
+        }
+        array[absValue - 1] *= -1;
     }
     return -1;
 }
