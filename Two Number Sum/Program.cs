2a714dc774ac2a25c6856307059d2c5b821a6e96
badruzzaman <badruzzaman@20220920CLONE>
2023-02-12 15:45:16 +0600

    g

--- Two Number Sum/Program.cs
+++ Two Number Sum/Program.cs
@@ -2,13 +2,14 @@
 
 int[] arr = { 1, 2, 3, 4, 5, 6 };
 int target = 9;
-int[] result = solution(arr, target);
+int[] result = solution_02(arr, target);
 foreach (var item in result)
 {
 	Console.Write(item + " ");
 }
 Console.ReadKey();
 
+// time O(N) space O(N)
 int[] solution(int[] arr, int target)
 {
     HashSet<int> set = new HashSet<int>();
@@ -26,4 +27,29 @@ int[] solution(int[] arr, int target)
 		}
     }
 	return new int[2];
+}
+
+// time O(NlongN) space O(1)
+int[] solution_02(int[] arr, int target)
+{
+    Array.Sort(arr);
+	int left = 0;
+	int right = arr.Length - 1;
+	while (left < right)
+	{
+		int currentNum = arr[left] + arr[right];
+		if(currentNum == target)
+		{
+			return new int[] { arr[left], arr[right] };
+		}
+	    else if (currentNum < target)
+		{
+			left++;	
+		}
+		else
+		{
+			right--;
+		}
+	}
+    return new int[2];
 }
