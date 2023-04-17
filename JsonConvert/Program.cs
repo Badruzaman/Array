90ce738f4e6ee1a3302442735903be3ffca3d96a
badruzzaman <badruzzaman@20220920CLONE>
2023-04-17 12:53:20 +0600

    sdfs

---
+++ JsonConvert/Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+using Newtonsoft.Json;
+
+Employee employee = new Employee { Id = 1, Name = "badru" };
+var result = JsonConvert.SerializeObject(employee);
+Console.WriteLine("Serialization ");
+Console.WriteLine(result);
+var deserialize = JsonConvert.DeserializeObject<Employee>(result);
+Console.WriteLine("Deserialization ");
+Console.WriteLine(deserialize.Id + " " + deserialize.Name);
+Console.ReadKey();
+
+class Employee
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+}
+
+
